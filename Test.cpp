2c89b94e73d7c98638a4a61d18d7d2f2fc1aa923
nik24 <nik240804@mail.ru>
2023-12-26 13:33:02 +0300

    Commit

---
+++ Test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    switch(operation) {
+        case '+':
+            std::cout << "Result: " << num1 + num2 << std::endl;
+            break;
+        case '-':
+            std::cout << "Result: " << num1 - num2 << std::endl;
+            break;
+        case '*':
+            std::cout << "Result: " << num1 * num2 << std::endl;
+            break;
+        case '/':
+            if(num2 != 0)
+                std::cout << "Result: " << num1 / num2 << std::endl;
+            else
+                std::cout << "Error: Division by zero is not possible." << std::endl;
+            break;
+        default:
+            std::cout << "Error: Invalid operation." << std::endl;
+            break;
+    }
+
+    return 0;
+}
